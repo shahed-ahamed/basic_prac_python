8631f886c30523753430be299eea26a2bc3708cb
Shahed Ahamed <shahedahamed.official@gmail.com>
2025-09-06 23:26:10 +0600

    Add files via upload

---
+++ logical_operator.py
@@ -0,0 +1,32 @@
+print (not False)  # True
+print (not True ) #False 
+
+a = 50
+b = 25
+print ( not (a>b)) # False
+
+val1 = True
+val2 = True
+
+print ("AND operator :", val1 and val2) # True
+
+print ("OR operator :", val1 or val2)   # True
+
+print ( "OR operator :", (a==b or a>b)) # True
+print ( "AND operator :", (a==b and a>b)) # False
+print ( "AND operator :", (a!=b and a>b)) # True
+print ( "AND operator :", (a!=b and a<b)) # False
+print ( "AND operator :", (a!=b and a>=b)) # True
+print ( "AND operator :", (a!=b and a<=b)) # False
+print ( "AND operator :", (a==b and a<=b)) # False
+print ( "AND operator :", (a==b and a>=b)) # False
+print ( "OR operator :", (a==b or a<b)) # False
+print ( "OR operator :", (a==b or a>=b)) # True
+print ( "OR operator :", (a!=b or a<b)) # True
+print ( "OR operator :", (a!=b or a>=b)) # True
+print ( "OR operator :", (a!=b or a>b)) # True
+print ( "OR operator :", (a==b or a>b)) # True
+print ( "OR operator :", (a==b or a<b)) # False
+print ( "OR operator :", (a==b or a>=b)) # True
+print ( "OR operator :", (a!=b or a<b)) # True
+print ( "OR operator :", (a!=b or a>=b)) # True
